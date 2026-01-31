a7f7f02fd2f21b76499a6508a54088229afd4ad8
Your Name <your-email@example.com>
2026-01-31 15:02:00 +0630

    Fix render deployment config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,11 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
-    // Render ကပေးတဲ့ URL အသန့်ကို ယူမယ်
+    // Render မှာ ပြင်ထားတဲ့ URL အသန့်ကို ယူပါမယ်
     url: process.env.DATABASE_URL!,
     
-    // ပြီးမှ SSL ကို ဒီနေရာမှာ Code နဲ့ သီးသန့်ဖွင့်မယ်
-    // ဒါဆိုရင် URL parsing error လုံးဝ မတက်တော့ပါဘူး
+    // SSL ကို ဒီနေရာမှာ Code နဲ့ သီးသန့်ဖွင့်ပေးလိုက်ပါ
+    // ဒါမှ Offset Error လုံးဝ မတက်တော့မှာပါ
     ssl: {
       rejectUnauthorized: true,
     },
